9628715f52f94863d2bb35cb4a3703d1037f8f89
marissagodwin <marissagodwin@Marissas-MacBook-Pro.local>
2015-03-09 18:48:07 -0400

    js tabs
    added tabs in js / html
    not yet styled

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 var $btnShowHide = $('.btn-show-hide');
 var $box = $('.box');
 
+var $tabs = $('.tabs');
+var $panels = $('.panel');
+
 $btnShowHide.on('click', function () {
     $box.toggleClass('js-show-hide') 
+});
+
+$tabs.on('click','a', function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href');  
+    
+   $panels.filter(':not([hidden])').attr('hidden', true);
+   $(id).removeAttr('hidden');
+    
+   $tabs.find('.js-current').removeClass('js-current');
+   $(this).addClass('js-current');
 });
