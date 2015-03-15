059148eaac6ca94397088411f755d09546e9fe34
marissagodwin <marissagodwin@Marissas-MacBook-Pro.local>
2015-03-15 10:13:05 -0400

    attempted lightbox
    attempted lightbox; failed, commented out html for later

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ var $box = $('.box');
 var $tabs = $('.tabs');
 var $panels = $('.panel');
 
+var $thumbs = $('.thumbnails');
+var $lbImg =$('.lightbox-img');
+var $lb =$('.lightbox');
+
 $btnShowHide.on('click', function () {
     $box.toggleClass('js-show-hide') 
 });
@@ -18,3 +22,10 @@ $tabs.on('click','a', function (e) {
    $tabs.find('.js-current').removeClass('js-current');
    $(this).addClass('js-current');
 });
+
+$thumbs.on('click','a', function (e){
+    e.preventDefault();
+    var big =$(this).attr('href')
+    $lbImg.attr('src', big);
+    $lb.attr('data-state','visible;');
+})
