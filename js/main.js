552bad2d56740985ad97d5642cbed7eed18b238c
marissagodwin <marissagodwin@marissas-mbp.phub.net.cable.rogers.com>
2015-03-28 19:13:03 -0400

    fixed scrolling
    Fixed scrolling js with much help from my homeboy Christian

--- js/main.js
+++ js/main.js
@@ -4,27 +4,23 @@ var $box = $('.box');
 var $tabs = $('.tabs');
 var $panels = $('.panel');
 
-
 $btnShowHide.on('click', function () {
-    $box.toggleClass('js-show-hide') 
+    $box.toggleClass('js-show-hide')
 });
 
 $tabs.on('click','a', function (e) {
     e.preventDefault();
-    var id = $(this).attr('href');  
-    
+    var id = $(this).attr('href');
+
    $panels.filter(':not([hidden])').attr('hidden', true);
    $(id).removeAttr('hidden');
-    
+
    $tabs.find('.js-current').removeClass('js-current');
    $(this).addClass('js-current');
 });
 
-$thumbs.on('click','a', function (e){
-    e.preventDefault();
-    var big =$(this).attr('href')
-    $lbImg.attr('src', big);
-    $lb.attr('data-state','visible;');
-})
+$.localScroll({
+  target: '.wrapper'
+});
 
-$('.toggleul').localScroll();
+
